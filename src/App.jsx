ec2ea58474fad26b0862c17d3b30949b8d06de1a
CodeKiShow <135666146+CodeKiShow@users.noreply.github.com>
2024-12-22 22:34:36 +0100

    Add basic App component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FridgeGenerator from './components/FridgeGenerator';
+
+function App() {
+  return (
+    <div>
+      <FridgeGenerator />
+    </div>
+  );
+}
+
+export default App;
